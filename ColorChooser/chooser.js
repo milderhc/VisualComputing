5b84d9e608346b437843250353640479b306c7f5
Milder Hernandez Cagua <milderhc@gmail.com>
2015-09-08 00:16:51 -0500

    ColorChooser first version

---
+++ ColorChooser/chooser.js
@@ -0,0 +1,33 @@
+var width = 400;
+var height = 400;
+var pickerSize = 100;
+var bottomSlider, leftSlider, hueSlider;
+
+function setup ( ) {
+    createCanvas(width, height);
+    noStroke();
+
+    hueSlider = createSlider(0, 360, 0);
+    hueSlider.position(20, pickerSize + 30);
+    saturationSlider = createSlider(0, 100, 0);
+    saturationSlider.position(20, pickerSize + 60);
+    brightnessSlider = createSlider(0, 100, 255);
+    brightnessSlider.position(20, pickerSize + 90);
+}
+
+
+function draw ( ) {
+    var g = saturationSlider.value();
+    text("Hue", 165, pickerSize + 35);
+    text("Saturation", 165, pickerSize + 65);
+    text("Brightness", 165, pickerSize + 95);
+
+    noStroke();
+    colorMode(HSB, pickerSize);
+    for (i = 0; i < pickerSize; i++) {
+        for (j = 0; j < pickerSize; j++) {
+            stroke(i, j, g);
+            point(i, j);
+        }
+    }
+}
